231d580510be77a68c39febe2a339f83569700fe
NLykoskoufis <nikolaslykos94@gmail.com>
2025-07-09 14:41:43 +0200

    removed unnecessary directories

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/nikolaoslykoskoufis/Personal/gtftools
-# Build directory: /Users/nikolaoslykoskoufis/Personal/gtftools/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/nikolaoslykoskoufis/Personal/gtftools/build/unit_tests[1]_include.cmake")
